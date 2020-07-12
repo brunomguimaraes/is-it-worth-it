ce08d375386a0d1b12a05fed3873295d83bdf073
Bruno <mguimaraesbruno@gmail.com>
2020-07-12 09:32:29 -0300

    add constants

---
+++ src/utils/constants/measures.tsx
@@ -0,0 +1,37 @@
+export const units = [
+  {
+    name: "unidade",
+    abbrev: "und",
+    multiplier: 1,
+  },
+  {
+    name: "gramas",
+    abbrev: "g",
+    multiplier: 1,
+  },
+  {
+    name: "kilogramas",
+    abbrev: "kg",
+    multiplier: 1000,
+  },
+  {
+    name: "litros",
+    abbrev: "l",
+    multiplier: 1,
+  },
+  {
+    name: "mililitros",
+    abbrev: "ml",
+    multiplier: 0.001,
+  },
+  {
+    name: "metro",
+    abbrev: "m",
+    multiplier: 1,
+  },
+  {
+    name: "centímetro",
+    abbrev: "cm",
+    multiplier: 0.01,
+  },
+];
