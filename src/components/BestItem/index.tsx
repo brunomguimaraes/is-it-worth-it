b104f4030f691c1d638d1d19a4d7f564aaa1ce9b
Bruno <mguimaraesbruno@gmail.com>
2020-07-12 15:17:53 -0300

    removes console.log

--- src/components/BestItem/index.tsx
+++ src/components/BestItem/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 import './styles.scss'
 import { Item } from '../RuleOfThree';
+import { position } from '../../utils/array';
 
 interface IBestItem {
   items: Item[]
@@ -11,20 +12,18 @@ function BestItem({ items }: IBestItem) {
 
   useEffect(() => {
     const minRatio = Math.min.apply(Math, items.map(function (obj) { return obj.ratio; }))
-    console.log('min', minRatio);
     const itemsWithMinRatio = items.filter(item => item.ratio === minRatio);
-    console.log('ITEMS?', itemsWithMinRatio);
     setBestItems(itemsWithMinRatio);
   }, [items]);
 
-  const getItemPosition = (item: Item, designatedItem: Item) => {
-    
+  const getItemPosition = (item: Item) => {
+    return position(items, item);
   };
 
-  console.log('THIS SHIT IS REAL?', bestItems)
   return (
     <div>
-      {bestItems && bestItems.length !== 0 && `O produto mais em conta tem preço igual a R$ ${bestItems[0]!.price}.`}
+      {bestItems && bestItems.length !== 0 && `O produto mais em conta é o ${getItemPosition(bestItems[0])} que tem preço igual a ${bestItems[0].price}.
+      Seu valor por unidade de medida é R$ ${bestItems[0].ratio.toFixed(2)}/${(bestItems[0].unit)}`}
     </div>
   );
 }
