cb59bf2a81fd5536379162c3ae9cf30b075c3243
Bruno <mguimaraesbruno@gmail.com>
2020-07-12 15:17:58 -0300

    adds type

--- src/utils/calculators.tsx
+++ src/utils/calculators.tsx
@@ -1,11 +1,12 @@
 import { units } from './constants/measures'
+import { Item } from '../components/RuleOfThree';
 
 const unitMultiplier = (unit: string) => {
   const selectedUnit = units.filter(u => u.abbrev === unit)
   return selectedUnit[0].multiplier
 };
 
-export const calculatePricePerUnit = (item: any) => {
+export const calculatePricePerUnit = (item: Item) => {
   const unitValue = item.weight * unitMultiplier(item.unit);
-  return (item.price / unitValue)
+  return (item.price / unitValue);
 }
