0276d33c0101515bae53f46e7a67edae475c5705
Bruno <mguimaraesbruno@gmail.com>
2020-07-13 14:58:11 -0300

    fix typo

--- src/components/Info/index.tsx
+++ src/components/Info/index.tsx
@@ -8,7 +8,7 @@ function Info() {
       <h1>Comprar produtos em quantidade nem sempre é o melhor negócio</h1>
       <h2>Embalagem tamanho família pode sair mais cara que o item avulso.</h2>
       <h3> É preciso sempre fazer as contas, a calculadora de mercado te ajuda!</h3>
-      <p>Todos queremos economizar, inspirado pelo mercado atacadista,
+      <p>Todos queremos economizar. Inspirado pelo mercado atacadista,
       normalmente o consumidor opta por embalagens econômicas de um único produto("packs, grades, etc")
       procurando sempre o melhor preço, no entanto, nem sempre isso é verdade.
       Existem casos em que as embalagens ditas "econômicas" saem mais caro do que o preço unitário do item.
